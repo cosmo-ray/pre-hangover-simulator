07fac2654815dfa9f4bb1e2361c14ab7d48f4b45
ArthurRobein <arthur.robein@gmail.com>
2017-11-23 10:53:14 -0500

    Multiple obstacles

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 local Q_KEY = 113
 local BASE_CHAR_SPEED = 10
 local BASE_SCROLL_SPEED = 5
+local OBSTACLE_DENSITY = 50
 
 function createStreetLine(wid, idx)
    ywCanvasNewObj(wid, 0, idx, 0)
@@ -14,45 +15,34 @@ function createStreetLine(wid, idx)
    ywCanvasNewObj(wid, 570, idx, 0)
 end
 
---[[function moveSaraBackup(entity, eve, objs)
-   local endRoad = yeGetInt(yeGet(entity, "road-end-idx"))
-   local saraPos = yeGet(yeGet(objs, endRoad), "pos")
-   if ywidEveKey(eve) == Y_UP_KEY then
-      saraPos = ywPosCreate(0, - BASE_CHAR_SPEED)
-      ywCanvasMoveObjByIdx(entity, endRoad, saraPos)
-      return
-   elseif ywidEveKey(eve) == Y_DOWN_KEY then
-      saraPos = ywPosCreate(0, BASE_CHAR_SPEED)
-      ywCanvasMoveObjByIdx(entity, endRoad, saraPos)
-      return
-   end 
-   if ywidEveKey(eve) == Y_LEFT_KEY then
-      saraPos = ywPosCreate(- BASE_CHAR_SPEED, 0)
-      ywCanvasMoveObjByIdx(entity, endRoad, saraPos)
-      return
-   elseif ywidEveKey(eve) == Y_RIGHT_KEY then
-      saraPos = ywPosCreate(BASE_CHAR_SPEED, 0)
-      ywCanvasMoveObjByIdx(entity, endRoad, saraPos)
-      return
-   end
-end]]
-
 function moveSara(entity, eve, objs)
    local endRoad = yeGetInt(yeGet(entity, "road-end-idx"))
-   local saraPos = yeGet(yeGet(objs, endRoad), "pos")
+   local saraIdx = ywCanvasIdxFromObj(entity, yeGet(entity, "sara"))
+   local saraPos = ywPosCreate(BASE_CHAR_SPEED * moveRight - BASE_CHAR_SPEED * moveLeft,
+			       BASE_CHAR_SPEED * moveDown - BASE_CHAR_SPEED * moveUp)
+   ywCanvasMoveObjByIdx(entity, saraIdx, saraPos)
+   yeDestroy(saraPos)
+end
 
-   saraPos = ywPosCreate(BASE_CHAR_SPEED * moveRight - BASE_CHAR_SPEED * moveLeft,
-			 BASE_CHAR_SPEED * moveDown - BASE_CHAR_SPEED * moveUp)
-   ywCanvasMoveObjByIdx(entity, endRoad, saraPos)   
+function checkColisions(entity)
+   local sara = yeGet(entity, "sara")
+   local touchBySara = ywCanvasNewColisionsArray(entity, sara)
+   for i = 0, yeLen(touchBySara) do
+      local touched = yeGet(touchBySara, i)
+      if yeGetInt(yeGet(touched, "type")) == 1 then
+	 print("she touch me")
+	 local pbs = ywCanvasObjSize(entity, yeGet(entity, "pukeBar"))
+	 print(pbs)
+	 ywPosSet(pbs, ywPosX(pbs) - 1, ywPosY(pbs))
+      end
+   end
+   --print(touchBySara, yeLen(touchBySara))
 end
 
 function phsAction(entity, eve, arg)
    local objs = yeGet(entity, "objs")
    local endRoad = yeGetInt(yeGet(entity, "road-end-idx"))
-
-   --   print("action !",
---	 yeGetString(yeGet(yeGet(yeGet(entity, "resources"), 0), "img")),
---	 yeGetInt(yeGet(entity, "road-end-idx")))
+   
    while ywidEveIsEnd(eve) == false do
       if ywidEveType(eve) == YKEY_DOWN and ywidEveKey(eve) == Q_KEY then yFinishGame() end
       if ywidEveType(eve) == YKEY_DOWN and ywidEveKey(eve) == Y_UP_KEY then moveUp = 1 end
@@ -64,23 +54,16 @@ function phsAction(entity, eve, arg)
       if ywidEveType(eve) == YKEY_UP and ywidEveKey(eve) == Y_LEFT_KEY then moveLeft = 0 end
       if ywidEveType(eve) == YKEY_UP and ywidEveKey(eve) == Y_RIGHT_KEY then moveRight = 0 end
       
-	    
---[[	 elseif ywidEveKey(eve) == Y_UP_KEY
-	    or ywidEveKey(eve) == Y_DOWN_KEY
-	    or ywidEveKey(eve) == Y_RIGHT_KEY
-	    or ywidEveKey(eve) == Y_LEFT_KEY
-	 then
-	    moveSara(entity, eve, objs)
-	 end
-   end]]
       eve = ywidNextEve(eve)
---      moveSara(entity, eve, objs)
    end
+   if turnNb % OBSTACLE_DENSITY == 0 then
+      createObstacle(entity)
+   end
+   moveObstacles(entity)
    moveSara(entity, eve, objs)
-   print("Up : ", moveUp)
+   checkColisions(entity)
    local pos = ywPosCreate(0, BASE_SCROLL_SPEED)
    local curPos = yeGet(yeGet(objs, 0), "pos")
---   ywPosPrint(curPos)
    yeDestroy(pos)
    if ywPosY(curPos) % 70 == 0 and ywPosY(curPos) > -70 then
       local pos = ywPosCreate(0, -65 - ywPosY(curPos))
@@ -92,9 +75,33 @@ function phsAction(entity, eve, arg)
       ywCanvasMoveObjByIdx(entity, idx, pos)
       idx = idx + 1
    end
-  -- ywCanvasMoveObjByIdx(entity, yeGetInt(yeGet(entity, "sara")), 0)
    yeDestroy(pos)
+   turnNb = turnNb +1
    return YEVE_ACTION
+end
+
+function moveObstacles(entity)
+   local obstacles = yeGet(entity, "obstacles")
+   local endObstacles = yeLen(obstacles)
+
+   for i = 0, endObstacles do
+      local obstacle = yeGet(obstacles, i)
+      local obstacleIdx = ywCanvasIdxFromObj(entity, obstacle)
+      local pos = ywPosCreate(0, BASE_SCROLL_SPEED)
+
+      if obstacleIdx ~= -1 then
+	 ywCanvasMoveObjByIdx(entity, obstacleIdx, pos)
+	 yeDestroy(pos)
+      end
+   end
+end
+
+function createObstacle(entity)
+   local garbage = ywCanvasNewObj(entity, yuiRand() % 600, -70, 5)
+
+   yeCreateInt(1, garbage, "type")
+   yePushBack(yeGet(entity, "obstacles"), garbage)
+   yeDestroy(garbage)
 end
 
 function initPhsWidget(entity)
@@ -102,47 +109,41 @@ function initPhsWidget(entity)
    moveDown = 0
    moveLeft = 0
    moveRight = 0
-
-   print("new wid", yeGet(entity, "<type>"))
+   turnNb = 0
+   
    yeCreateString("rgba: 0 0 0 255", entity, "background")
    yeCreateFunction("phsAction", entity, "action")
-   yeCreateInt(150000, entity, "turn-length")
+   yeCreateInt(100000, entity, "turn-length")
    local objs = yeCreateArray(entity, "objs");
 
    local screenH = 480
    local idx = -70;
    while idx < screenH do
       createStreetLine(entity, idx)
-      --[[local obj = yeCreateArray(objs)
-      ywPosCreate(0, idx, obj, "pos")
-      yeCreateInt(0, obj, "id")
-      obj = yeCreateArray(objs)
-      ywPosCreate(70, idx, obj, "pos")
-      yeCreateInt(1, obj, "id")
-      jdx = 140
-      while jdx < 500 do
-	 obj = yeCreateArray(objs)
-	 ywPosCreate(jdx, idx, obj, "pos")
-	 yeCreateInt(3, obj, "id")
-	 jdx = jdx + 60
-      end
-      obj = yeCreateArray(objs)
-      ywPosCreate(500, idx, obj, "pos")
-      yeCreateInt(2, obj, "id")
-      obj = yeCreateArray(objs)
-      ywPosCreate(570, idx, obj, "pos")
-	 yeCreateInt(0, obj, "id")--]]
       idx = idx + 70
    end
    yeCreateInt(yeLen(objs), entity, "road-end-idx")
-   ywCanvasNewObj(entity, 350, 250, 4)
---   yeCreateInt(yeLen(objs), entity, "sara")
+   local sara = ywCanvasNewObj(entity, 350, 250, 4)
+   yePushBack(entity, sara, "sara")
+   yeCreateArray(entity, "obstacles")
+   
+   createObstacle(entity)
+
+   local obj = yeCreateArray(objs)
+   yeCreateInt(1, obj)
+   ywPosCreate(10, 10, obj)
+   local rect = yeCreateArray(obj)
+   ywPosCreate(100, 10, rect)
+   yeCreateString("rgba: 180 50 10 160", rect)
+   yePushBack(entity, obj, "pukeBar")
+
    local canvas = ywidNewWidget(entity, "canvas")
    return canvas
 end
 
 function init(mod)
    local map = yeCreateArray(mod, "game")
+   yuiRandInit()
    yeCreateString("phs", map, "<type>")
    yePushBack(map, ygGet("phs.resources"), "resources")
    local init = yeCreateArray()
